ff32674c25530992a33d64de948bfd941d9fdea9
Tom Soares <tomsoares.souza@gmail.com>
2017-09-06 11:15:31 -0300

    Adicionando rotas para consulta por CEP

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('cep/{cep}/sync', ['uses' => 'LocationController@getLocationByCepSync']);
+$router->get('cep/{cep}/async', ['uses' => 'LocationController@getLocationByCepASync']);
